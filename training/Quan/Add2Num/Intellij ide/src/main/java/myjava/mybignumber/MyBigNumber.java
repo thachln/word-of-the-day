f4a6f8500ab51ea25065eda72d1a3f01da6abd14
nguyenchauthaoquan <51600072@student.tdtu.edu.vn>
2018-12-25 15:05:53 +0700

    MyBigNumber v12.0

--- training/Quan/Add2Num/Intellij ide/src/main/java/myjava/mybignumber/MyBigNumber.java
+++ training/Quan/Add2Num/Intellij ide/src/main/java/myjava/mybignumber/MyBigNumber.java
@@ -46,12 +46,12 @@ public class MyBigNumber {
         int length1 = s1.length();// do dai chuoi thu 1
         int length2 = s2.length();// do dai chuoi thu 2
         final int max = (length1 > length2) ? length1 : length2;// lay do dau lon nhat giua a va b
-        int nho = 0;// Khởi tạo số nhớ = 0 để khi cộng sẽ có vài trường hợp lớn hơn 9
+        int remember = 0;// Khởi tạo số nhớ = 0 để khi cộng sẽ có vài trường hợp lớn hơn 9
         int pos1 = 0;// Vị trí chuỗi s1
         int pos2 = 0;// Vị trí chuỗi s2
         char c1;// kí tự c1 dùng để lấy kí tự cuối cùng của chuỗi s1
         char c2;// kí tự c2 dùng để lấy kí tự cuối cùng của chuỗi s2
-        int tong = 0;// Khởi tạo biến tổng = 0 để cộng 2 kí tự cuối cùng lại với nhau
+        int sum = 0;// Khởi tạo biến tổng = 0 để cộng 2 kí tự cuối cùng lại với nhau
         // Dùng Matcher kết hợp với pattern để tìm các kí tự đặc biết trong 2 chuỗi
         Pattern pattern = Pattern.compile("[:/!@#$%&*^()_+=|<>?{}\\\\[\\\\]~-]");
         final Matcher matcher1 = pattern.matcher(s1);
@@ -100,17 +100,18 @@ public class MyBigNumber {
             c1 = (pos1 >= 0) ? s1.charAt(length1 - i - 1) : '0';
             c2 = (pos2 >= 0) ? s2.charAt(length2 - i - 1) : '0';
 
-            tong = (c1 - '0') + (c2 - '0') + nho;// chuyển kí tự thành số xong cộng cho số nhớ
-            result = (tong % 10) + result;// Lấy kết quả tổng ở trên chia lấy dư cho 10 và ghép vào chuỗi kết quả
-            nho = tong / 10;// Cập nhật lại số nhớ
+            sum = (c1 - '0') + (c2 - '0') + remember;// chuyển kí tự thành số xong cộng cho số nhớ
+            result = (sum % 10) + result;// Lấy kết quả tổng ở trên chia lấy dư cho 10 và ghép vào chuỗi kết quả
+            remember = sum / 10;// Cập nhật lại số nhớ
 
             msg = "Step " + (i + 1) + ": " + c1 + " + " + c2 + " = "
-                    + (tong - nho) + " + " + nho + " = " + tong + " . Write " + (tong % 10) + " remember " + nho;
+                    + (sum - remember) + " + " + remember + " = " + sum
+                    + " . Write " + (sum % 10) + " remember " + remember;
             this.ireceiver.send(msg);
         }
 
-        if (nho > 0) {
-            result = nho + result;// Nếu số nhớ còn dư thì ghép vào chuỗi kết quả
+        if (remember > 0) {
+            result = remember + result;// Nếu số nhớ còn dư thì ghép vào chuỗi kết quả
         }
 
         return result;// Trả về kết quả thu được
