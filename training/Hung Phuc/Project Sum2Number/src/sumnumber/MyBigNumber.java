b8f1e071ab55020c97827b606b850e42e7bc4d87
headhuntez98 <43946896+headhuntez98@users.noreply.github.com>
2018-12-28 19:59:49 +0700

    Add files via upload

--- training/Hung Phuc/Project Sum2Number/src/sumnumber/MyBigNumber.java
+++ training/Hung Phuc/Project Sum2Number/src/sumnumber/MyBigNumber.java
@@ -4,33 +4,35 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 /**
- * This is my code about how to sum 2 number in string and return result in string.
- *
+ * This is my code about how to sum 2 number in string , user input s1 and s2. 
+ *  
  * @author phucnguyen.
  *
  * @version 3.0.
  */
+
+
 public class MyBigNumber {
-
+    
     private IReceiver ireceiver;
-
+    
     public MyBigNumber(final IReceiver ireceiver) {
-
+    
         this.ireceiver = ireceiver;
     }
-
+    
     /**
      * This is function for sum.
      *
      * @param s1 is first string.
      * @param s2 is second string.
-     * 
+     * @return sum of s1 and s2.
      */
-    public String sum(final String s1, final String s2) {
-
-        int len1 = s1.length();
+    
+    public String sum(final String s1,final String s2) { 
+        int len1 = s1.length(); 
         int len2 = s2.length();
-        final int maxLen = Math.max(len1, len2);
+        final int maxLen = Math.max(len1,len2);
         char c1; //to save char of digit of s1
         char c2; //to save char of digit of s2
         int d1; //value of c1 in integer
@@ -43,38 +45,51 @@ public class MyBigNumber {
         String msg = ""; //to print message 
         int index1;
         int index2;
-
-        //Code to check user input
-        //Check if user enter special char
         Pattern pattern1 = Pattern.compile("[!@#$%&*()_+=|<>?{}\\\\[\\\\]~-]");
         final Matcher matcher1 = pattern1.matcher(s1);
-
         Pattern pattern2 = Pattern.compile("[!@#$%&*()_+=|<>?{}\\\\[\\\\]~-]");
         final Matcher matcher2 = pattern1.matcher(s2);
 
-        ////Check if user enter negative number  
+        //Code to check user input
+
+        ////Check if user enter negative number
+        if (len1 > 0) {
+            if (s1.charAt(0) == '-') {
+                throw new NumberFormatException("s1 contains negative number: " + s1);
+            }
+        }
+
+        if (len2 > 0) {
+            if (s2.charAt(0) == '-') {
+                throw new NumberFormatException("s2 contains negative number: " + s2);
+            }
+        }
+
+        ////Check if user enter letter
         for (int i = 0; i < len1; i++) {
             if (Character.isLetter(s1.charAt(i))) {
-                throw new NumberFormatException("Position " + (i + 1) + " in String " + s1
-                        + " not a number");
+                throw new NumberFormatException("Position " + (i + 1) + " in s1 is a letter ");
             }
         }
-
         for (int i = 0; i < len2; i++) {
             if (Character.isLetter(s2.charAt(i))) {
-                throw new NumberFormatException("Position" + (i + 1) + " in String " + s2
-                        + " not a number");
+                throw new NumberFormatException("Position " + (i + 1) + " in s2 is a letter ");
             }
         }
-
+        
+        //Special
+        //Check if user enter special char
+        
+        
+        
         if (matcher1.find()) {
             throw new NumberFormatException("Position " + (matcher1.start() + 1) + " in String " + s1
-                    + " not a number");
+            + " not a number");
         }
 
         if (matcher2.find()) {
             throw new NumberFormatException("Position " + (matcher2.start() + 1) + " in String " + s2
-                    + " not a number");
+            + " not a number");
         }
 
         ////Check user input empty string
@@ -82,20 +97,23 @@ public class MyBigNumber {
 
             return s2;
         }
+        
         if (!s1.isEmpty() && s2.isEmpty()) {
 
             return s1;
         }
+        
         if (s1.isEmpty() && s2.isEmpty()) {
 
             return "0";
         }
 
         ////////This Code for sum
+
         for (int i = 0; i < maxLen; i++) {
             index1 = len1 - i - 1; //to choose what char index in s1 we will take
             index2 = len2 - i - 1; //to choose what char index in s2 we will take
-
+ 
             c1 = (index1 >= 0) ? s1.charAt(index1) : '0';
             c2 = (index2 >= 0) ? s2.charAt(index2) : '0';
 
@@ -106,20 +124,20 @@ public class MyBigNumber {
 
             finalResult = (tmp % 10) + finalResult; // plus with finalResult by add it before finalResult
 
-            msg = "step " + step + ": " + d1 + " + " + d2 + " = " + (d1 + d2) + " + "
-                    + mem + " = " + tmp + " . Write " + (tmp % 10) + " remember " + (tmp / 10);
+            msg = "Step " + step + ": " + d1 + " + " + d2 + " = " + (d1 + d2) + " + " 
+                + mem + " = " + tmp + " . Write " + (tmp % 10) + " remember " + (tmp / 10);
             this.ireceiver.send(msg);
 
-            step++; //increase step
-
+            step ++; //increase step
+            
             mem = tmp / 10; // Change mem
         }
 
         //// when we run all length and we still have mem = 1 , so we write 1 at first of result
-        if (mem == 1) {
+        if (mem == 1) { 
             finalResult = 1 + finalResult;
 
-            msg = "step " + step + ": Write down 1 and result is " + finalResult;
+            msg = "Step " + step + ": Write down 1 and result is " + finalResult; 
             this.ireceiver.send(msg);
         }
 
